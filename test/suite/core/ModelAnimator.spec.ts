28424e2301dda9763c63a7dbe8bea1a1edf51847
WoodNeck <saiyu915@naver.com>
2022-02-14 14:25:27 +0900

    test: fix failing tests

--- test/suite/core/ModelAnimator.spec.ts
+++ test/suite/core/ModelAnimator.spec.ts
@@ -1,41 +1,50 @@
 import * as THREE from "three";
 
 import ModelAnimator from "~/core/ModelAnimator";
+import { createView3D } from "../../test-utils";
 
 describe("ModelAnimator", () => {
   describe("Initial state", () => {
-    it("should have mixer on it", () => {
-      expect(new ModelAnimator(new THREE.Scene()).mixer).to.be.instanceOf(THREE.AnimationMixer);
+    it("should have mixer on it", async () => {
+      const view3D = await createView3D();
+      expect(new ModelAnimator(view3D).mixer).to.be.instanceOf(THREE.AnimationMixer);
     });
 
-    it("should have clips as an empty array", () => {
-      expect(new ModelAnimator(new THREE.Scene()).clips).to.deep.equal([]);
+    it("should have clips as an empty array", async () => {
+      const view3D = await createView3D();
+      expect(new ModelAnimator(view3D).clips).to.deep.equal([]);
     });
 
-    it("should have actions as an empty array", () => {
-      expect(new ModelAnimator(new THREE.Scene()).actions).to.deep.equal([]);
+    it("should have actions as an empty array", async () => {
+      const view3D = await createView3D();
+      expect(new ModelAnimator(view3D).actions).to.deep.equal([]);
     });
 
-    it("should have animationCount as 0", () => {
-      expect(new ModelAnimator(new THREE.Scene()).animationCount).to.equal(0);
+    it("should have animationCount as 0", async () => {
+      const view3D = await createView3D();
+      expect(new ModelAnimator(view3D).animationCount).to.equal(0);
     });
 
-    it("should have activeAnimation as null", () => {
-      expect(new ModelAnimator(new THREE.Scene()).activeAnimation).to.equal(null);
+    it("should have activeAnimation as null", async () => {
+      const view3D = await createView3D();
+      expect(new ModelAnimator(view3D).activeAnimation).to.equal(null);
     });
 
-    it("should have activeAnimationIndex as -1", () => {
-      expect(new ModelAnimator(new THREE.Scene()).activeAnimationIndex).to.equal(-1);
+    it("should have activeAnimationIndex as -1", async () => {
+      const view3D = await createView3D();
+      expect(new ModelAnimator(view3D).activeAnimationIndex).to.equal(-1);
     });
 
-    it("is not paused", () => {
-      expect(new ModelAnimator(new THREE.Scene()).paused).to.be.false;
+    it("is not paused", async () => {
+      const view3D = await createView3D();
+      expect(new ModelAnimator(view3D).paused).to.be.false;
     });
   });
 
   describe("setClips", () => {
-    it("should add the given clips", () => {
-      const animator = new ModelAnimator(new THREE.Scene());
+    it("should add the given clips", async () => {
+      const view3D = await createView3D();
+      const animator = new ModelAnimator(view3D);
       const clip = new THREE.AnimationClip("", 0, []);
 
       animator.setClips([clip]);
@@ -43,8 +52,9 @@ describe("ModelAnimator", () => {
       expect(animator.clips).to.deep.equal([clip]);
     });
 
-    it("should set actions with the given clips", () => {
-      const animator = new ModelAnimator(new THREE.Scene());
+    it("should set actions with the given clips", async () => {
+      const view3D = await createView3D();
+      const animator = new ModelAnimator(view3D);
       const clip = new THREE.AnimationClip("", 0, []);
 
       animator.setClips([clip]);
@@ -55,8 +65,9 @@ describe("ModelAnimator", () => {
   });
 
   describe("reset", () => {
-    it("should reset clips & actions", () => {
-      const animator = new ModelAnimator(new THREE.Scene());
+    it("should reset clips & actions", async () => {
+      const view3D = await createView3D();
+      const animator = new ModelAnimator(view3D);
       const clip = new THREE.AnimationClip("", 0, []);
       animator.setClips([clip]);
 
